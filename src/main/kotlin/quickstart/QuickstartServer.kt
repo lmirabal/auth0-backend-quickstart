443d7dd48bc55128b1200604b7bd1e48e061bf37
Luis Mirabal <lmirabal@gmail.com>
2020-05-10 13:53:52 +0100

    Introduce public endpoint
    Extract http handler to separate the http layer from its transport. This
    would allow testing the app in memory but given the simplicity of the
    app, the testing is run against an http server instance.

--- src/main/kotlin/quickstart/QuickstartServer.kt
+++ src/main/kotlin/quickstart/QuickstartServer.kt
@@ -1,10 +1,24 @@
 package quickstart
 
-import org.http4k.core.Request
+import org.http4k.core.HttpHandler
+import org.http4k.core.Method.GET
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
+import org.http4k.core.then
+import org.http4k.filter.DebuggingFilters
+import org.http4k.routing.bind
+import org.http4k.routing.routes
 import org.http4k.server.Http4kServer
 import org.http4k.server.Jetty
 import org.http4k.server.asServer
 
-fun quickstartServer(port: Int) : Http4kServer = { _: Request -> Response(OK) }.asServer(Jetty(port))
+fun quickstartServer(port: Int): Http4kServer = quickstartApp().asServer(Jetty(port))
+
+fun quickstartApp(): HttpHandler =
+    DebuggingFilters.PrintRequestAndResponse()
+        .then(
+            routes(
+                "/ping" bind GET to { Response(OK) },
+                "/api/public" bind GET to { Response(OK).body("Hello from a public endpoint!")}
+            )
+        )
